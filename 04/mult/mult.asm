ceb2faa1384549fb0c1c60a635b0dd74981ad0e0
wyh <708949379@qq.com>
2019-10-21 13:57:23 +0800

    finished asm1

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,20 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2
+M=0
+(LOOP)
+@R0
+D=M-1
+M=D
+@END
+D;JLT
+@R2
+D=M
+@R1
+D=D+M
+@R2
+M=D
+@LOOP
+0;JMP
+(END)
