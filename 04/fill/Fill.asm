fe29500311453e1d3140dbd5cd4e8a22d5c83d91
= <=>
2019-10-23 16:25:57 +0800

    finished chapter 04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -14,80 +14,50 @@
 // Put your code here.
 @24576
 D=A
-@MXSCREEN
+@MAXSCREEN
 M=D
 
-
-// define screen pointer (16384)
 @SCREEN
 D=A
 @POINTER
 M=D
 
-
-(LOOP)     // infinite loop
-  // If we have keyboard input, jump to FILL
-  @KBD
-  D=M
-  @FILL
-  D; JGT
-
-  // Otherwise jump to UNFILL
-  @UNFILL
-  0; JMP
-
-
-(UNFILL)
-  // Do nothing if POINTER == SCREEN
-  @SCREEN
-  D=A
-  @POINTER
-  D=D-M
-  @LOOP
-  D; JEQ
-
-  // Unfill the screen
-  D=0
-  @POINTER
-  A=M
-  M=D
-
-  // Decrement POINTER
-  D=1
-  @POINTER
-  D=M-D
-  @POINTER
-  M=D
-
-  // Jump back to main loop
-  @LOOP
-  0; JMP
-
+(LOOP)
+@KBD
+D=M
+@FILL
+D;JGT
+@UNFILL
+D;JEQ
 
 (FILL)
-  // Do nothing if the screen is full
-  @MXSCREEN
-  D=M
-  @POINTER
-  D=D-M
-  @LOOP
-  D; JEQ
+@POINTER
+D=M
+@MAXSCREEN
+D=D-M
+@LOOP
+D;JEQ
+@POINTER
+A=M
+M=-1
+D=A+1
+@POINTER
+M=D
+@LOOP
+0;JMP
 
-
-  // Fill in the pixel that POINTER points to
-  //@-1
-  D=-1
-  @POINTER
-  A=M
-  M=D
-
-  // Iterate pointer by 1
-  D=1
-  @POINTER
-  D=D+M
-  @POINTER
-  M=D
-
-  // jump back to main loop
-  @LOOP
-  0; JMP
+(UNFILL)
+@POINTER
+D=M
+@SCREEN
+D=D-A
+@LOOP
+D;JEQ
+@POINTER
+A=M
+M=0
+D=A-1
+@POINTER
+M=D
+@LOOP
+0;JMP
